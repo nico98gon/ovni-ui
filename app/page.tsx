a3f1ded55d2cfe30dbba159130ebd42af50cdf46
Nicolas Gonzalez <nicodevelo@gmail.com>
2024-10-17 19:45:36 -0300

    panda config fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { css } from "@/styled-system/css";
 
 export default function Home() {
   return (
-    <div className={css({ fontSize: "2xl", fontWeight: 'bold', display: 'flex', justifyContent: 'center', alignItems: 'center' })}>
+    <div className={css({ fontSize: "2xl", fontWeight: 'bold', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: '#e0e0e0' })}>
       <p>
         Hello 🐼!
       </p>
